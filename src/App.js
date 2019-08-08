b45aa5a9125ee7fd064413d0ead32c54ec7a77cd
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-08 13:38:22 -0700

    components connected

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todos.component";
+import TodosList from './components/todos-list.component';
+
 class App extends Component {
   render() {
   return (
     <Router>
     <div className="container">
       <h2>MERN Stack Todo App</h2>
-    </div>
     <Route path="/" component={TodosList} />
     <Route path="/edit/:id" component={EditTodo} />
     <Route path="/create" component={CreateTodo} />
+    </div>
     </Router>
   );
   }
