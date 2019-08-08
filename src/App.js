54331ee455a674753d6405983b01aaf1a52b9af2
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-08 13:28:09 -0700

    added routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 class App extends Component {
   render() {
   return (
+    <Router>
     <div className="container">
       <h2>MERN Stack Todo App</h2>
     </div>
+    <Route path="/" component={TodosList} />
+    <Route path="/edit/:id" component={EditTodo} />
+    <Route path="/create" component={CreateTodo} />
+    </Router>
   );
   }
 }
