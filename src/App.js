42f20e10a5011c58b7e57cf1a986c09cb70890d3
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-08 14:04:20 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todos.component";
 import TodosList from './components/todos-list.component';
 
+import logo from "./logo.png";
+
 class App extends Component {
   render() {
   return (
     <Router>
-    <div className="container">
-      <h2>MERN Stack Todo App</h2>
-      <Route path="/home" component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
-    </div>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="https://github.com/walkerfloyd1" target="_blank">
+            <img src={logo} width="30" height="30" alt="Github link"/>          
+          </a>
+          <Link to="/home" className="navbar-brand">MERN Stack To Do App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/home" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/home" component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </div>
     </Router>
   );
   }
